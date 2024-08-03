9acdee17912e8a1fa343355be4202b97b2e1e459
Pratibhamore01 <pratibhamore2004@gmail.com>
2024-08-03 23:08:48 +0530

    find the sum of odd no

--- index.js
+++ index.js
@@ -11,5 +11,20 @@ for (const num of marks)
 }
 console.log(largest)
 
+//Program to find the sum of odd numbers from the given array.
 
- 
+const number = [ 3,6,4,5,9,2 ];
+console.log(number)
+let sum=0;
+
+for(let i=0 ; i<number.length ; i++)
+{
+    if(number[i]% 2==1)
+    {
+        sum += number[i];
+    }
+    
+}
+console.log (`Sum of a odd numbers are ${sum}`)
+
+
